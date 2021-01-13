f0541cfe1e48ca4be6b13cc5b350a9c28469c1f4
Simon Treanor <Simon.Treanor@fundourselves.com>
2021-01-13 17:45:39 +0000

    Improved error handling

--- FunStripe/RestApi.fs
+++ FunStripe/RestApi.fs
@@ -9,14 +9,43 @@ module RestApi =
         Error: ErrorObject
     }
 
-    ///Record for detailing response errors
+    ///Record for detailing response errors; see [Stripe API docs](https://stripe.com/docs/api/errors)
     and ErrorObject = {
+        ///For card errors, the ID of the failed charge.
+        Charge: string option
+        ///For some errors that could be handled programmatically, a short string indicating the [error code](https://stripe.com/docs/error-codes) reported.
         Code: string
+        ///For card errors resulting from a card issuer decline, a short string indicating the [card issuer’s reason for the decline](https://stripe.com/docs/declines#issuer-declines) if they provide one.
+        DeclineCode: string option
+        ///A URL to more information about the [error code](https://stripe.com/docs/error-codes) reported.
         DocUrl: string
+        ///A human-readable message providing more details about the error. For card errors, these messages can be shown to your users.
         Message: string
+        Metadata: Map<string, string>
+        ///If the error is parameter-specific, the parameter related to the error. For example, you can use this to display a message near the correct form field.
         Param: string
-        Type: string
+        ///The PaymentIntent object for errors returned on a request involving a PaymentIntent.
+        PaymentIntent: StripeModel.PaymentIntent option
+        ///The PaymentMethod object for errors returned on a request involving a PaymentMethod.
+        PaymentMethod: StripeModel.PaymentMethod option
+        ///If the error is specific to the type of payment method, the payment method type that had a problem. This field is only populated for invoice-related errors.
+        PaymentMethodType: StripeModel.PaymentMethodType option
+        ///The SetupIntent object for errors returned on a request involving a SetupIntent.
+        SetupIntent: StripeModel.SetupIntent option
+        ///The source object for errors returned on a request involving a source.
+        Source: StripeModel.Source option
+        ///The type of error returned. One of `api_connection_error`, `api_error`, `authentication_error`, `card_error`, `idempotency_error`, `invalid_request_error`, `or rate_limit_error`
+        Type: ErrorType
     }
+
+    and ErrorType =
+    | ApiConnectionError
+    | ApiError
+    | AuthenticationError
+    | CardError
+    | IdempotencyError
+    | InvalidRequestError
+    | RateLimitError
 
     ///Class for making API calls to the Stripe API
     type RestApiClient(?baseUrl: string, ?apiKey: string, ?idempotencyKey: string, ?stripeAccount: string, ?stripeVersion: string) =
